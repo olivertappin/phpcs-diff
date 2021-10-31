f74353aa74ead59019afce583389edc7706b07e8
Kevin Hamilton <kevin@eagleeye.com>
2021-10-31 15:33:50 +0000

    Unit tests for PhpcsDiff and ViolationsMapper. Add ext-gettext to composer, as it is used

--- tests/Mapper/PhpcsViolationsMapperTest.php
+++ tests/Mapper/PhpcsViolationsMapperTest.php
@@ -8,6 +8,44 @@ use PhpcsDiff\Tests\TestBase;
 
 class PhpcsViolationsMapperTest extends TestBase
 {
+    private $mappedData1 = [
+        'file.php' => [
+            'messages' => [
+                [
+                    'line' => 100,
+                    'message' => 'This is the message',
+                    'type' => 'ERROR',
+                ]
+            ],
+        ],
+    ];
+
+    private $mappedData2 = [
+        'SomeImportantClass.php' => [
+            'messages' => [
+                [
+                    'line' => 230,
+                    'message' => 'This is the message',
+                    'type' => 'ERROR',
+                ],
+            ],
+        ],
+        'index.php' => [
+            'messages' => [
+                [
+                    'line' => 230,
+                    'message' => 'This is the message',
+                    'type' => 'ERROR',
+                ],
+                [
+                    'line' => 250,
+                    'message' => 'Some other warning',
+                    'type' => 'WARNING',
+                ]
+            ],
+        ],
+    ];
+
     /**
      * @covers \PhpcsDiff\Mapper\PhpcsViolationsMapper::__construct
      */
@@ -27,5 +65,61 @@ class PhpcsViolationsMapperTest extends TestBase
         $mappedData = (new PhpcsViolationsMapper([], ''))->map([]);
 
         $this->assertEmpty($mappedData);
+    }
+
+    /**
+     * @covers \PhpcsDiff\Mapper\PhpcsViolationsMapper::__construct
+     * @covers \PhpcsDiff\Mapper\PhpcsViolationsMapper::map
+     */
+    public function testMapperLineMatch(): void
+    {
+        $changedLinesPerFile = [
+            'file.php' => [
+                100,
+            ]
+        ];
+
+        $mappedData = (new PhpcsViolationsMapper($changedLinesPerFile, ''))->map($this->mappedData1);
+
+        self::assertIsArray($mappedData);
+        self::assertCount(1, $mappedData);
+        self::assertIsString($mappedData[0]);
+        self::assertSame($mappedData[0], 'file.php' . PHP_EOL . ' - Line 100 (ERROR) This is the message' . PHP_EOL);
+    }
+
+    /**
+     * @covers \PhpcsDiff\Mapper\PhpcsViolationsMapper::__construct
+     * @covers \PhpcsDiff\Mapper\PhpcsViolationsMapper::map
+     */
+    public function testMapperNoLineMatch(): void
+    {
+        $changedLinesPerFile = [
+            'file.php' => [
+                101,
+            ]
+        ];
+
+        $mappedData = (new PhpcsViolationsMapper($changedLinesPerFile, ''))->map($this->mappedData1);
+
+        self::assertIsArray($mappedData);
+        self::assertCount(0, $mappedData);
+    }
+
+    /**
+     * @covers \PhpcsDiff\Mapper\PhpcsViolationsMapper::__construct
+     * @covers \PhpcsDiff\Mapper\PhpcsViolationsMapper::map
+     */
+    public function testMapperNoFileMatch(): void
+    {
+        $changedLinesPerFile = [
+            'NonMatchedClass.php' => [
+                101,
+            ]
+        ];
+
+        $mappedData = (new PhpcsViolationsMapper($changedLinesPerFile, ''))->map($this->mappedData2);
+
+        self::assertIsArray($mappedData);
+        self::assertCount(0, $mappedData);
     }
 }
